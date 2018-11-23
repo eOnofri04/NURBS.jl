db6128539b545b07dcbf597e47bfa78145e805a4
pmacciacchera <polmacra@outlook.it>
2018-11-23 12:26:44 +0100

    il commento

--- src/rbsurfaces.jl
+++ src/rbsurfaces.jl
@@ -23,8 +23,42 @@ Calculate a Cartesian product rational B-spline surface (NURBS) using an open un
 Call: basis, knot, sumrbas
 """
 
-function rbspsurf(b, k, l, npts, mpts, p1, p2)::Array{Float64}
-	[...]
+function rbspsurf(B::Array{Float64},k::Int64,l::Int64,npts::Int64,mpts::Int64,p1::Int64,p2::Int64)::Array{Float64}
+    
+    nplusc = npts + k
+    mplusc = mpts+l
+    x = zeros(nplusc)
+    y = zeros(mplusc)
+    nbasis = zeros(1,npts)
+    mbasis = zeros(1,mpts)
+    q = zeros(p1*p2,3)
+    
+    #generate the open uniform knot vectors
+    knot(npts,k,x)
+    knot(mpts,l,y)
+   
+    icount = 0
+    #calculate the points on the B-spline surface
+    stepu = x[nplusc]/(p1-1)
+    stepw = y[mplusc]/(p2-1)
+    for u = 0:stepu:x[nplusc]
+        basis(k,u,npts,x,nbasis)
+        for w = 0:stepw:y[mplusc]
+            basis(l,w,mpts,y,mbasis)
+            sumrbas(B,nbasis,mbasis,npts,mpts)
+            icount = icount+1
+            for i = 1:npts
+                for j = 1:mpts
+                    for s = 1:3
+                        j1 = mpts * (i-1) + j
+                        qtemp = (B[j1,4] * B[j1,s] * nbasis[i] * mbasis[j]) / sum
+                        q[icount,s] = q[icount,s] + qtemp
+                    end
+                end
+            end
+        end
+    end
+    return(q)
 end
 
 #-----------------------------------------------------------------------
@@ -35,6 +69,13 @@ end
 Calculate the sum of the nonrational basis functions.
 """
 
-function sumrbas(b::Array{Float64}, nbasis::Array{Float64}, mbasis::Array{Float64}, npts::Int64, mpts::Int64)::Float64
-	[...]
+function sumrbas(B::Array{Float64},nbasis::Array{Float64},mbasis::Array{Float64},npts::Int64,mpts::Int64)::Float64
+    sum = 0
+    for i = 1:npts
+        for j = 1:mpts
+            j1 = mpts * (i-1) + j
+            sum = sum + B(j1,n) * nbasis[i] * mbasis[j]
+        end
+    end
+    return(sum)
 end
