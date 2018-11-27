950a1caefaca394a56efeef93ee88d2612058a64
Elia Onofri <30669145+eOnofri04@users.noreply.github.com>
2018-11-27 12:10:13 +0100

    Update knot.jl

--- src/knot.jl
+++ src/knot.jl
@@ -2,24 +2,24 @@ export knot, knotc, knotu
 
 
 """
-    knot(n1, k, centre=false, step=1.0)
-Generate a `k` order B-spline open uniform knot vector over `n1 = n+1` control vertices.
+    knot(npts, ord, centre=false, step=1.0)
+Generate a `ord` order B-spline open uniform knot vector over `npts = n+1` control vertices.
 
 An open uniform knot vector has multiplicity of knot values
- at the ends equals to the order `k` of the B-Spline basis function.
+ at the ends equals to the order `ord` of the B-Spline basis function.
 Internal knot values are evenly spaced (uniform).
 The resulting open uniform basis functions yield curves
  that behave most nearly like Bézier curves.
 `centre` Boolean value indicates if the knots are centered in the
- zero value or if them goes from zero to `m-1` where `m=n1+k`.
+ zero value or if them goes from zero to `m-1` where `m=npts+ord`.
  If `true` is choosen, then `m` must be odd.
-Necessary condition is that `n1>=k` in order to correct build the knot vector.
+Necessary condition is that `npts>=ord` in order to correct build the knot vector.
 
 ---
 
 # Arguments
-- `n1::Int64`: the number of the `n+1` points of the controll polygon.
-- `k::Int64`: the order of the B-Spline (`degree k-1`).
+- `npts::Int64`: the number of the `n+1` points of the controll polygon.
+- `ord::Int64`: the order of the B-Spline (`degree k-1`).
 - `centre::Bool=false`: if the knot is centered in zero.
 - `step::Float64=1.0`: the step between the knots.
 
@@ -69,32 +69,26 @@ julia> knot(5, 2, true)
 _By Elia Onofri_
 """
 
-function knot(n1::Int64, k::Int64, centre::Bool=false, step::Float64=1.0)::Array{Float64}
-    local x = Float64[]                # knot vector output
-    local m::Int64 = n1+k              # knot vector dimension
-    local backstep::Float64 = 0.0      # backstep is the affine transition parameter for 0-central knots
-    
+function knot(npts::Int64, ord::Int64, centre::Bool=false, step::Float64=1.0)::Array{Float64}
+    # Input Data Check
+    @assert npts>=ord ("ERROR: ord > npts")
+	
+    local m::Int64 = npts+ord      		   # knot vector dimension
+    local backstep::Float64 = 0.0   		   # backstep is the affine transition parameter for 0-central knots
+    local x::Array{Float64} = Array{Float64}(m)    # knot vector output
+	
     # Different preparation for centre knot vector
     if centre
-        if m%2!=1
-            print("ERROR: no centring points available")
-            return x
-        else
-            backstep = step*(n1-k+1)/2
-        end
+        @assert m % 2 == 1 ("ERROR: no centring points available")
+        backstep = step * (npts - ord + 1) / 2
     end
     
-    # Local data check
-    if n1<k
-        print("WARNING: k > n1")
-    end
-    
-    push!(x, 0-backstep)
+    x[1] = 0 - backstep
     for i = 2:m
-        if i>k && i<=n1+1
-            push!(x, last(x)+step)
+        if i > ord && i <= npts+1
+            x[i] = x[i-1] + step
         else
-            push!(x, last(x))
+            x[i] = x[i-1]
         end
     end
     return x
@@ -119,8 +113,8 @@ end
 
 
 """
-    knotu(n1, k, step=1.0)
-Generate a B-spline periodic uniform knot vector over `n` control vertices.
+    knotu(npts, ord, step=1.0)
+Generate a B-spline periodic uniform knot vector over `npts = n+1` control vertices.
 
 In a periodic uniform knot vector the knot values are evenly spaced by `step`.
 Moreover each base is a translate of the others as it could be defined like:
@@ -132,8 +126,8 @@ Moreover each base is a translate of the others as it could be defined like:
 ---
 
 # Arguments
-- `n1::Int64`: the number of the `n+1` points of the controll polygon.
-- `k::Int64`: the order of the B-Spline (`degree k-1`).
+- `npts::Int64`: the number of the `n+1` points of the controll polygon.
+- `ord::Int64`: the order of the B-Spline (`deg = ord-1`).
 - `step::Float64=1.0`: the step between the knots.
 
 ---
@@ -169,17 +163,15 @@ julia> knotu(5, 4, 0.25)
 _By Elia Onofri_
 """
 
-function knotu(n1::Int64, k::Int64, step::Float64=1.0)::Array{Float64}
-    x=Float64[];          # knot vector output
-    m=n1+k;             # knot vector dimension
-
-    # Local data check
-    if n1<k
-        print("WARNING: k > n1")
-    end
+function knotu(npts::Int64, ord::Int64, step::Float64=1.0)::Array{Float64}
+    # Input Data Check
+    @assert npts>=ord ("ERROR: ord > npts")
+	
+    local m::Int64 = npts+ord      		   # knot vector dimension
+    local x::Array{Float64} = Array{Float64}(m)    # knot vector output
 
     for i=1:m
-        push!(x, (i-1)*step);
+        x[i] = (i-1)*step
     end
 
     return x;
