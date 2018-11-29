e675250fadeb8cd3361298570410a8b6ff1b9655
Elia Onofri <30669145+eOnofri04@users.noreply.github.com>
2018-11-29 15:51:20 +0100

    Update basis.jl

--- src/basis.jl
+++ src/basis.jl
@@ -1,263 +1,346 @@
 export basis, dbasis, dbasisu
 
-
 """
-	basis(c, t, npts, x)
+    basis(ord, t, npts, x[])
 
-Generate B-spline basis functions for open uniform knot vectors.
+Generate a B-spline basis functions `N[]` for a given open knot vector `x[]`.
+
+A B-Spline basis is a collection of functions of a parameter `t` wich form
+ a basis for the vectorial space of functions. The conformation of this set
+ higly depends on the choosing of the knots `x[]` the curve is bound to.
+The basis is computed with _Cox-de Boor_ recursive function applied to the
+ basis dependency tree in order to optimise the computation.
+
+---
+
+# Arguments
+- `ord::Int64`: the order of the B-Spline (`deg = ord-1`).
+- `t::Float64`: the parameter value of the parametric curve.
+- `npts::Int64`: the number of the points of the controll polygon.
+- `x::Array{Float64}`: the knot vector.
+
+---
+
+_By Elia Onofri_
 """
 
-function basis(c::Int64,t::Float64,npts::Int64,x::Array{Float64},n::Array{Float64})
-    nplusc=npts+c    
-    temp=zeros(nplusc)
+function basis(ord::Int64, t::Float64, npts::Int64, x::Array{Float64})::Array{Float64}
+    local tmp = Float64[]       # Basis progressive vector
+    local N = Float64[]         # Output vector
+    local max_N = npts-1+ord    # Needs i+(ord-1)|i=npts = npts-1+ord trivial basis
+	local m::Int64 = npts+ord	# Dimension of the knot vector
+    local ddep::Float64 = 0.0   # Direct Dependency partial sum
+    local fdep::Float64 = 0.0   # Forward Dependency partial sum
     
+    # Local check of the knot vector correctness
+    @assert length(x) == m ("ERROR: incompatibile knot vector with given parameters n+1 = $(npts), k = $(ord)")
     
-#calculate the first-order basis functions Ni,1
-for i=1:nplusc-1
-    if t>=x[i] && t<x[i+1]
-        temp[i]=1
-    else
-        temp[i]=0
-    end
-end
-
-    
-##calculate the higher-order basis functions
-for k=2:c
-    for i=1:nplusc-k
-        if temp[i]!=0   #if basis function is zero skip the calculation
-            d=((t-x[i])*temp[i])/(x[i+k-1]-x[i])
+    # Eval N_{i,1} for i = 1:max_B
+    for i = 1:max_N
+        if (t>=x[i]) && (t<x[i+1])
+            append!(tmp, 1)
         else
-            d=0
-        end
-        if temp[i+1]!=0   #if basis function is zero skip the calculation
-            e=((x[i+k]-t)*temp[i+1])/(x[i+k]-x[i+1])
-        else
-            e=0
-        end
-        temp[i]=d+e
-    end
-end
-
-if t==x[nplusc]  #pick up last point
-    temp[npts]=1
-end
-
-    
-#put in n array
-for i=1:npts
-    n[i]=temp[i]
-end
-
-
-if t==x[nplusc]    #pick up last point
-    n[npts]=1
-end
-
-end
-
-#-----------------------------------------------------------------------
-
-"""
-	dbasis(c, t, npts, x)
-
-Generate B-spline basis functions and their derivatives for uniform open knot vectors.
-"""
-
-function dbasis(c::Int64,t::Float64,npts::Int64,x::Array{Float64},n::Array{Float64},d1::Array{Float64},d2::Array{Float64})
-    nplusc=npts+c
-    #zero the temporary arrays
-    temp=zeros(nplusc)
-    temp1=zeros(nplusc)
-    temp2=zeros(nplusc)
-    #calculate the first-order basis functions n(i,1)
-    for i=1:nplusc-1
-        if t>=x[i] && t<x[i+1]
-            temp[i]=1
-        else
-            temp[i]=0
+            append!(tmp, 0)
         end
     end
     
-    if t==x[nplusc]  temp[npts]=1 end
-    
-    #calculate higher-order basis functions and their derivatives
-    
-    for k=2:c
-        for i=1:nplusc-k
-            
-            #calculate basis function
-            if temp[i]!=0
-                b1=((t-x[i])*temp[i])/(x[i+k-1]-x[i])
+    # Eval higher basis N_{i,deg} for deg = 2:ord and i = 1:max_B-deg
+    for deg = 2:ord
+        for i = 1:m-deg
+            # Eval of the direct dependency
+            if tmp[i]==0
+                ddep = 0.0
             else
-                b1=0
+                ddep = ((t-x[i])*tmp[i])/(x[i+deg-1]-x[i])
             end
-            if temp[i+1]!=0
-                b2=((x[i+k]-t)*temp[i+1])/(x[i+k]-x[i+1])
+            # Eval of the forward dependency
+            if tmp[i+1]==0
+                fdep = 0.0
             else
-                b2=0
+                fdep = ((x[i+deg]-t)*tmp[i+1])/(x[i+deg]-x[i+1])
             end
-            
-            #calculate first derivative
-            if temp[i]!=0
-                f1=temp[i]/(x[i+k-1]-x[i])
-            else
-                f1=0
-            end
-            if temp[i+1]!=0
-                f2=-temp[i+1]/(x[i+k]-x[i+1])
-            else
-                f2=0
-            end
-            if temp1[i]!=0
-                f3=((t-x[i])*temp1[i])/(x[i+k-1]-x[i])
-            else
-                f3=0
-            end
-            if temp1[i+1]!=0
-                f4=((x[i+k]-t)*temp1[i+1])/(x[i+k]-x[i+1])
-            else
-                f4=0
-            end
-            
-            #calculate second derivative
-            if temp1[i]!=0
-                s1=(2*temp1[i])/(x[i+k-1]-x[i])
-            else
-                s1=0
-            end
-            if temp1[i+1]!=0
-                s2=(-2*temp1[i+1])/(x[i+k]-x[i+1])
-            else
-                s2=0
-            end
-            if temp2[i]!=0
-                s3=((t-x[i])*temp2[i])/(x[i+k-1]-x[i])
-            else
-                s3=0
-            end
-            if temp2[i+1]!=0
-                s4=((x[i+k]-t)*temp2[i+1])/(x[i+k]-x[i+1])
-            else
-                s4=0
-            end
-            
-            temp[i]=b1+b2
-            temp1[i]=f1+f2+f3+f4
-            temp2[i]=s2+s2+s3+s4
+            # Collection of the dependencies
+            tmp[i] = ddep+fdep
         end
     end
     
-    #put in arrays
+    # Otherwise last point is zero
+    if t == x[m]
+        tmp[npts] = 1
+    end
+    
+    # Collect N{1,ord} to N{npts,ord} in B
     for i=1:npts
-        n[i]=temp[i]
-        d1[i]=temp1[i]
-        d2[i]=temp2[i]
+        push!(N, tmp[i]);
     end
-            
-    
+    return N;
 end
-#-----------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------------------------------------
+
 
 """
-	dbasisu(c, t, npts, x)
+    dbasis(ord, t, npts, x[])
 
-Generate B-spline basis functions and their derivatives for uniform periodic knot vectors.
+Generate a B-spline basis functions `N[]` and its derivatives `d1[], d2[]` for a given open knot vector `x[]`.
+
+A B-Spline basis is a collection of functions of a parameter `t` wich form
+ a basis for the vectorial space of functions. The conformation of this set
+ higly depends on the choosing of the knots `x[]` the curve is bound to.
+The basis is computed with _Cox-de Boor_ recursive function applied to the
+ basis dependency tree in order to optimise the computation.
+
+---
+
+# Arguments
+- `ord::Int64`: the order of the B-Spline (`deg = ord-1`).
+- `t::Float64`: the parameter value of the parametric curve.
+- `npts::Int64`: the number of the points of the controll polygon.
+- `x::Array{Float64}`: the knot vector.
+
+---
+
+_By Elia Onofri_
 """
 
-function dbasisu(c::Int64,t::Float64,npts::Int64,x::Array{Float64},n::Array{Float64},d1::Array{Float64},d2::Array{Float64})
-    nplusc=npts+c
-    #zero the temporary arrays
-    temp=zeros(nplusc)
-    temp1=zeros(nplusc)
-    temp2=zeros(nplusc)
-    #calculate the first-order basis functions n(i,1)
-    for i=1:nplusc-1
-        if t>=x[i] && t<x[i+1]
-            temp[i]=1
+function dbasis(ord::Int64, t::Float64, npts::Int64, x::Array{Int64})::Tuple{Array{Float64}, Array{Float64}, Array{Float64}}
+    local tmp = Float64[]       		# Basis progressive vector
+    local tmp1 = Float64[]      		# First Derivate progressive vector
+    local tmp2 = Float64[]     			# Second Derivate progressive vector
+    local N = Float64[]        			# Basis vector
+	local d1 = Float64[]        		# First derivative vector
+	local d2 = Float64[]        		# Second derivative vector
+    local max_N::Int64 = npts-1+ord		# Needs i+(ord-1)|i=npts = npts-1+ord trivial basis
+	local m::Int64 = npts+ord			# Dimension of the knot vector
+    local ddep::Float64 = 0.0   		# Direct Dependency partial sum
+    local fdep::Float64 = 0.0   		# Forward Dependency partial sum
+    
+    # Local check of the knot vector correctness
+    @assert length(x) == m ("ERROR: incompatibile knot vector with given parameters n+1 = $(npts), k = $(ord)")
+    
+    # Eval N_{i,1} for i = 1:max_B
+    for i = 1:max_N
+        if (t >= x[i]) && (t < x[i+1])
+            append!(tmp, 1)
         else
-            temp[i]=0
+            append!(tmp, 0)
         end
     end
     
-    if t==x[npts+1]
-        temp[npts]=1
-        temp[npts+1]=0
+    if t == x[m]    # last(x)
+        temp[npts] = 1;
     end
     
-    #calculate higher-order basis functions and their derivatives
-    
-    for k=2:c
-        for i=1:nplusc-k
+	# Eval higher basis N_{i,deg} for deg = 2:ord and i = 1:max_B-deg
+    for deg = 2:ord
+        for i = 1:m-deg
             
-            #calculate basis function
-            if temp[i]!=0
-                b1=((t-x[i])*temp[i])/(x[i+k-1]-x[i])
+			# ----Eval of the direct dependency----
+            if tmp[i] == 0
+                ddep = 0.0
             else
-                b1=0
-            end
-            if temp[i+1]!=0
-                b2=((x[i+k]-t)*temp[i+1])/(x[i+k]-x[i+1])
-            else
-                b2=0
+                ddep = ((t - x[i]) * tmp[i]) / (x[i+deg-1] - x[i])
             end
             
-            #calculate first derivative
-            if temp[i]!=0
-                f1=temp[i]/(x[i+k-1]-x[i])
+			# ----Eval of the forward dependency----
+            if tmp[i+1] == 0
+                fdep = 0.0
             else
-                f1=0
+                fdep = ((x[i+deg] - t) * tmp[i+1]) / (x[i+deg] - x[i+1])
             end
-            if temp[i+1]!=0
-                f2=-temp[i+1]/(x[i+k]-x[i+1])
+			
+			#----calculate first derivative----
+            if tmp[i] != 0    # if the lower order basis function is zero skip the calculation
+                f1 = tmp[i] / (x[i+deg-1] - x[i]);
             else
-                f2=0
+                f1 = 0.0;
             end
-            if temp1[i]!=0
-                f3=((t-x[i])*temp1[i])/(x[i+k-1]-x[i])
+            if tmp[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                f2 = -tmp[i+1] / (x[i+deg] - x[i+1]);
             else
-                f3=0
+                f2 = 0.0;
             end
-            if temp1[i+1]!=0
-                f4=((x[i+k]-t)*temp1[i+1])/(x[i+k]-x[i+1])
+            if tmp1[i] != 0    # if the lower order basis function is zero skip the calculation
+                f3 = ((t - x[i]) * tmp1[i]) / (x[i+deg-1] - x[i]);
             else
-                f4=0
+                f3 = 0.0;
             end
-            
-            #calculate second derivative
-            if temp1[i]!=0
-                s1=(2*temp1[i])/(x[i+k-1]-x[i])
+            if tmp1[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                f4 = ((x[i+deg] - t) * tmp1[i+1]) / (x[i+deg] - x[i+1]);
             else
-                s1=0
+                f4 = 0.0;
             end
-            if temp1[i+1]!=0
-                s2=(-2*temp1[i+1])/(x[i+k]-x[i+1])
+			
+			#----calculate second derivative----
+			if tmp1[i] != 0    # if the lower order basis function is zero skip the calculation
+                s1 = (2 * tmp1[i]) / (x[i+deg-1] - x[i]);
             else
-                s2=0
+                s1 = 0;
             end
-            if temp2[i]!=0
-                s3=((t-x[i])*temp2[i])/(x[i+k-1]-x[i])
+            if tmp1[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                s2 = ((-2) * tmp1[i+1]) / (x[i+deg] - x[i+1]);
             else
-                s3=0
+                s2 = 0;
             end
-            if temp2[i+1]!=0
-                s4=((x[i+k]-t)*temp2[i+1])/(x[i+k]-x[i+1])
+            if tmp2[i] != 0    # if the lower order basis function is zero skip the calculation
+                s3 = ((t - x[i]) * tmp2[i] ) / (x[i+deg-1]-x[i]);
             else
-                s4=0
+                s3 = 0;
             end
-            
-            temp[i]=b1+b2
-            temp1[i]=f1+f2+f3+f4
-            temp2[i]=s2+s2+s3+s4
+            if tmp2[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                s4 = ((x[i+deg] - t) * tmp2[i+1]) / (x[i+deg] - x[i+1]);
+            else
+                s4 = 0;
+            end
+			
+            # Collection of the dependencies
+            tmp[i] = ddep + fdep
+			tmp1[i] = f1 + f2 + f3 + f4;
+            tmp2[i] = s1 + s2 + s3 + s4;
         end
     end
     
-    #put in arrays
+    # prepare output
     for i=1:npts
-        n[i]=temp[i]
-        d1[i]=temp1[i]
-        d2[i]=temp2[i]
+        push!(N, tmp[i]);
+        push!(d1, tmp1[i]);
+        push!(d2, tmp2[i]);
     end
-            
+    return (N, d1, d2);
+end
+
+
+#--------------------------------------------------------------------------------------------------------------------------------
+
+
+"""
+    dbasisu(ord, t, npts, x[])
+
+Generate a B-spline basis functions `N[]` and its derivatives `d1[], d2[]` for a given uniform periodic knot vector `x[]`.
+
+A B-Spline basis is a collection of functions of a parameter `t` wich form
+ a basis for the vectorial space of functions. The conformation of this set
+ higly depends on the choosing of the knots `x[]` the curve is bound to.
+The basis is computed with _Cox-de Boor_ recursive function applied to the
+ basis dependency tree in order to optimise the computation.
+
+---
+
+# Arguments
+- `ord::Int64`: the order of the B-Spline (`deg = ord-1`).
+- `t::Float64`: the parameter value of the parametric curve.
+- `npts::Int64`: the number of the points of the controll polygon.
+- `x::Array{Float64}`: the knot vector.
+
+---
+
+_By Elia Onofri_
+"""
+
+function dbasis(ord::Int64, t::Float64, npts::Int64, x::Array{Int64})::Tuple{Array{Float64}, Array{Float64}, Array{Float64}}
+    local tmp = Float64[]       		# Basis progressive vector
+    local tmp1 = Float64[]      		# First Derivate progressive vector
+    local tmp2 = Float64[]     			# Second Derivate progressive vector
+    local N = Float64[]        			# Basis vector
+	local d1 = Float64[]        		# First derivative vector
+	local d2 = Float64[]        		# Second derivative vector
+    local max_N::Int64 = npts-1+ord		# Needs i+(ord-1)|i=npts = npts-1+ord trivial basis
+	local m::Int64 = npts+ord			# Dimension of the knot vector
+    local ddep::Float64 = 0.0   		# Direct Dependency partial sum
+    local fdep::Float64 = 0.0   		# Forward Dependency partial sum
     
+    # Local check of the knot vector correctness
+    @assert length(x) == m ("ERROR: incompatibile knot vector with given parameters n+1 = $(npts), k = $(ord)")
+    
+    # Eval N_{i,1} for i = 1:max_B
+    for i = 1:max_N
+        if (t >= x[i]) && (t < x[i+1])
+            append!(tmp, 1)
+        else
+            append!(tmp, 0)
+        end
+    end
+    
+    if t==x[npts+1]    # handle the end specially
+        tmp[npts] = 1;    # resetting the first order basis functions.
+        tmp[npts+1] = 0;
+    end
+    
+	# Eval higher basis N_{i,deg} for deg = 2:ord and i = 1:max_B-deg
+    for deg = 2:ord
+        for i = 1:m-deg
+            
+			# ----Eval of the direct dependency----
+            if tmp[i] == 0
+                ddep = 0.0
+            else
+                ddep = ((t - x[i]) * tmp[i]) / (x[i+deg-1] - x[i])
+            end
+            
+			# ----Eval of the forward dependency----
+            if tmp[i+1] == 0
+                fdep = 0.0
+            else
+                fdep = ((x[i+deg] - t) * tmp[i+1]) / (x[i+deg] - x[i+1])
+            end
+			
+			#----calculate first derivative----
+            if tmp[i] != 0    # if the lower order basis function is zero skip the calculation
+                f1 = tmp[i] / (x[i+deg-1] - x[i]);
+            else
+                f1 = 0.0;
+            end
+            if tmp[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                f2 = -tmp[i+1] / (x[i+deg] - x[i+1]);
+            else
+                f2 = 0.0;
+            end
+            if tmp1[i] != 0    # if the lower order basis function is zero skip the calculation
+                f3 = ((t - x[i]) * tmp1[i]) / (x[i+deg-1] - x[i]);
+            else
+                f3 = 0.0;
+            end
+            if tmp1[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                f4 = ((x[i+deg] - t) * tmp1[i+1]) / (x[i+deg] - x[i+1]);
+            else
+                f4 = 0.0;
+            end
+			
+			#----calculate second derivative----
+			if tmp1[i] != 0    # if the lower order basis function is zero skip the calculation
+                s1 = (2 * tmp1[i]) / (x[i+deg-1] - x[i]);
+            else
+                s1 = 0;
+            end
+            if tmp1[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                s2 = ((-2) * tmp1[i+1]) / (x[i+deg] - x[i+1]);
+            else
+                s2 = 0;
+            end
+            if tmp2[i] != 0    # if the lower order basis function is zero skip the calculation
+                s3 = ((t - x[i]) * tmp2[i] ) / (x[i+deg-1]-x[i]);
+            else
+                s3 = 0;
+            end
+            if tmp2[i+1] != 0    # if the lower order basis function is zero skip the calculation
+                s4 = ((x[i+deg] - t) * tmp2[i+1]) / (x[i+deg] - x[i+1]);
+            else
+                s4 = 0;
+            end
+			
+            # Collection of the dependencies
+            tmp[i] = ddep + fdep
+			tmp1[i] = f1 + f2 + f3 + f4;
+            tmp2[i] = s1 + s2 + s3 + s4;
+        end
+    end
+    
+    # prepare output
+    for i=1:npts
+        push!(N, tmp[i]);
+        push!(d1, tmp1[i]);
+        push!(d2, tmp2[i]);
+    end
+    return (N, d1, d2);
 end
