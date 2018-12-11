df6fe77ef9dfd26dd72480b5ca8b5e4528f0079d
eOnofri04 <elia.onofri@mensa.it>
2018-12-11 15:10:08 +0100

    correzione al file di test, ora funzionante

--- src/NURBS.jl
+++ src/NURBS.jl
@@ -1,20 +1,19 @@
 module NURBS
-	#import ...
-	export bezier, dbezier
+	#export bezier, dbezier
 	export knot, knotu, knotc
-	export basis, dbasis, dbasisu
-	export bspline, bsplineu, dbspline, dbsplineu
-	export rbasis, rbspline, rbsplinu
-	export bezsurf, mbezsurf
-	export bsplsurf, bspsurfu, dbsurf
-	export frbsurf, rbspsurf
+	#export basis, dbasis, dbasisu
+	#export bspline, bsplineu, dbspline, dbsplineu
+	#export rbasis, rbspline, rbsplinu
+	#export bezsurf, mbezsurf
+	#export bsplsurf, bspsurfu, dbsurf
+	#export frbsurf, rbspsurf
 
-	include("bezier.jl")
+	#include("bezier.jl")
 	include("knot.jl")
-	include("basis.jl")
-	include("bspline.jl")
-	include("rbspline.jl")
-	include("beziersurfaces.jl")
-	include("bsurfaces.jl")
-	include("rbsurfaces.jl")
+	#include("basis.jl")
+	#include("bspline.jl")
+	#include("rbspline.jl")
+	#include("beziersurfaces.jl")
+	#include("bsurfaces.jl")
+	#include("rbsurfaces.jl")
 end
