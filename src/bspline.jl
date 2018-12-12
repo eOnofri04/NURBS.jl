29f77728860bd1f69a1c8a164ccd53c81fc50558
Elia Onofri <30669145+eOnofri04@users.noreply.github.com>
2018-12-12 22:32:41 +0100

    Update bspline.jl

--- src/bspline.jl
+++ src/bspline.jl
@@ -86,7 +86,7 @@ end
 
 
 """
-UNTESTED	nmatrix(k)
+	nmatrix(k)
 
 Calculate the general B-spline periodic basis matrix.
 
@@ -117,24 +117,7 @@ _By Paolo Macciacchera, Elia Onofri_
 """
 
 function nmatrix(k::Int64)::tuple{Float64, Array{Int64}}
-    Ni = (n,i) -> factorial(n) / (factorial(i) * factorial(n-i)) 
-    n::Array{Int64} = Array{Int64}(zeros(ord,ord))
-    fcoeff = 1 / factorial(ord - 1)
-    
-    for i = 0 : ord-1
-        temp = Ni(ord-1, i)
-        for j = 0 : ord-1
-            sum = 0
-            for k = j : ord-1
-                sum1 = (ord - (k + 1)) ^ i
-                sum2 = (-1) ^ (k - j)
-                sum3 = Ni(ord, k - j)
-                sum = sum + sum1 * sum2 * sum3
-            end
-            n[i+1, j+1] = temp * sum
-        end
-    end
-    return(fcoeff, n)            
+    [...]           
 end
 
 
@@ -143,7 +126,7 @@ end
 
 
 """
-UNTESTED	param(dpts, d[])
+	param(dpts, d[])
 
 Calculate parameter values based on chord distances.
 
@@ -173,19 +156,7 @@ _By Paolo Macciacchera, Elia Onofri_
 """
 
 function param(dpts::Int64, d::Array{Float64})::Array{Float64}
-    sum::Float64 = 0
-    isum::Float64 = 0
-    tparm::Array{Float64} = Array{Float64}(zeros(dpts))
-    #calculate the sum of the chord distances for all the data points
-    for i = 2 : dpts
-        sum = sum + sqrt((d[i,1] - d[i-1,1]) ^ 2 + (d[i,2] - d[i-1,2]) ^ 2)
-    end
-    #calculate the parameter values
-    for i = 2 : dpts
-        isum = isum + sqrt((d[i,1] - d[i-1,1]) ^ 2 + (d[i,2] - d[i-1,2]) ^ 2)
-        tparm[i] = isum / sum
-    end
-    return(tparm)
+    [...]
 end
 
 
