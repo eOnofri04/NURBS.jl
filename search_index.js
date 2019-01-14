7087e1f96576f63def2f8acfb75aea840595be4c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-14 12:46:05 +0000

    Deploy eOnofri04/NURBS.jl to github.com/eOnofri04/NURBS.jl.git:gh-pages

--- search_index.js
+++ search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "NURBS.jl",
     "category": "section",
-    "text": "NURBS.jl (or Non Uniform Rational B-Spline) is a Julia library to build parametrical curves and parametrical surface based on the interpolation of polynomial bases. This library is developed by:Elia Onofri - Ln (elia.onofri4@gmail.com)\nGianmarco Caramitti (g.caramitti@gmail.com)\nPaolo Macciacchera (polmacra@outlook.it)\nGiuseppe Santorelli (giu.santorelli15@gmail.com)and would be maintained by the Computational Visual Design Laboratory (CVDLAB) of Università degli Studi di Roma Tre."
+    "text": "NURBS.jl (or Non Uniform Rational B-Spline Surfaces) is a Julia library to build parametrical curves and parametrical surface based on the interpolation of polynomial bases. This library is developed by:Elia Onofri - Ln (elia.onofri4@gmail.com)\nGianmarco Caramitti (g.caramitti@gmail.com)\nPaolo Macciacchera (polmacra@outlook.it)\nGiuseppe Santorelli (giu.santorelli15@gmail.com)and would be maintained by the Computational Visual Design Laboratory (CVDLAB) of Università degli Studi di Roma Tre."
 },
 
 {
@@ -57,17 +57,17 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "bezierC.html#Bezier-Curves-1",
+    "location": "bezierC.html#Bézier-Curves-1",
     "page": "Bezier Curves",
-    "title": "Bezier Curves",
+    "title": "Bézier Curves",
     "category": "section",
-    "text": "Bezier Curves are a special kind of NURBS curves build upon a controll polygon B and a Bernstein basis J_ni.Mathematically, a parametric Bézier curve is defined byP(t) = sum_i=0^n B_iJ_ni(t) quad 0 leq t leq 1Where the Bernstein Basis is defined asJ_n i(t) = binom ni t^i(1-t)^n-iwhere the convention (0)^0 = 1 and 0 = 1 have been made.In particular J_ni is the i-th base function of order n, while n is also the number of segments of the the controll polygon (number of points minus one)."
+    "text": "Bézier Curves are a special kind of NURB curves build upon a controll polygon B and a Bernstein basis J_ni.Mathematically, a parametric Bézier curve is defined byP(t) = sum_i=0^n B_iJ_ni(t) quad 0 leq t leq 1Where the Bernstein Basis is defined asJ_n i(t) = binom ni t^i(1-t)^n-iwhere the convention (0)^0 = 1 and 0 = 1 have been made.In particular J_ni is the i-th base function of order n, while n is also the number of segments of the the controll polygon (number of points minus one)."
 },
 
 {
-    "location": "bezierC.html#Bezier-Properties-1",
+    "location": "bezierC.html#Bézier-Properties-1",
     "page": "Bezier Curves",
-    "title": "Bezier Properties",
+    "title": "Bézier Properties",
     "category": "section",
     "text": "Whe have a small variety of properties, descending directly from the definition:Base function are often real.\nThe degree of the polynomial curve is one less than the number of point of the controll polygon.\nThe curve generally follows the shape of the control polygon.\nThe edges of the curves are the edges of the control polygon.\nThe curve is located inside the convex hull of the controll polygon.\nThe curve is invariant under Affine transformation."
 },
@@ -77,15 +77,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Bezier Curves",
     "title": "Matrix Representation",
     "category": "section",
-    "text": "The equation of a Bézier Curve could also be implemented as a Matrix Multiplication (particulary usefull in GPU computations).P(t) = FGwhereF = J_n0 dots J_n n	qquad	G^t = B_0 dots B_nMoreover is possible to collect the coefficient of the Basis in a square matrix N in mathcal M_nn(mathbb R) obtaining:P(t) = t^n t^n-1 dots t 1 N G = TNGwhereN = beginbmatrix\n	binom n0binom nn (-1)^n  binom n1 binomn-1n-1(-1)^n-1  dots  binom nn binomn-nn-n(-1)^0\n	binom n0binom nn-1 (-1)^n-1  binom n1 binomn-1n-2(-1)^n-2  dots  0\n	vdots  vdots  ddots  vdots\n	binom n0binom n1 (-1)^1  binom n1 binomn-10(-1)^0  dots  0\n	binom n0binom n0 (-1)^0  0  dots  0\nendbmatrixIt is also possible to decompose the matrix N even further in the product of two matrices:N = CD quad Rightarrow quad P(t) = TCDGwhereN = beginbmatrix\n	binom nn (-1)^n  binomn-1n-1(-1)^n-1  dots  binomn-nn-n(-1)^0\n	binom nn-1 (-1)^n-1  binomn-1n-2(-1)^n-2  dots  0\n	vdots  vdots  ddots  vdots\n	binom n1 (-1)^1  binomn-10(-1)^0  dots  0\n	binom n0 (-1)^0  0  dots  0\nendbmatrixN = beginbmatrix\n	binom n0  0  dots  0\n	0  binom n1  dots  0\n	vdots  vdots  ddots  vdots\n	0  0  dots  binom nn\nendbmatrix"
+    "text": "The equation of a Bézier Curve could also be implemented as a Matrix Multiplication (particulary usefull in GPU computations).P(t) = FGwhereF = J_n0 dots J_n n	qquad	G^t = B_0 dots B_nMoreover is possible to collect the coefficient of the Basis in a square matrix N in mathcal M_nn(mathbb R) obtaining:P(t) = t^n t^n-1 dots t 1 N G = TNGwhereN = beginbmatrix\n	binom n0binom nn (-1)^n  binom n1 binomn-1n-1(-1)^n-1  dots  binom nn binomn-nn-n(-1)^0\n	binom n0binom nn-1 (-1)^n-1  binom n1 binomn-1n-2(-1)^n-2  dots  0\n	vdots  vdots  ddots  vdots\n	binom n0binom n1 (-1)^1  binom n1 binomn-10(-1)^0  dots  0\n	binom n0binom n0 (-1)^0  0  dots  0\nendbmatrixor, in other words:left(N_i+1j+1right)_ij=0^n = begincases\n		binom nj binomn-jn-i-j(-1)^n-i-j		mboxif  0 leq i+j leq n\n		0												mboxotherwise\n	endcasesIt is also possible to decompose the matrix N even further in the product of two matrices:N = CD quad Rightarrow quad P(t) = TCDGwhereC = beginbmatrix\n	binom nn (-1)^n  binomn-1n-1(-1)^n-1  dots  binomn-nn-n(-1)^0\n	binom nn-1 (-1)^n-1  binomn-1n-2(-1)^n-2  dots  0\n	vdots  vdots  ddots  vdots\n	binom n1 (-1)^1  binomn-10(-1)^0  dots  0\n	binom n0 (-1)^0  0  dots  0\nendbmatrixD = beginbmatrix\n	binom n0  0  dots  0\n	0  binom n1  dots  0\n	vdots  vdots  ddots  vdots\n	0  0  dots  binom nn\nendbmatrix"
 },
 
 {
-    "location": "bezierC.html#Bezier-Derivatives-1",
+    "location": "bezierC.html#Bézier-Derivatives-1",
     "page": "Bezier Curves",
-    "title": "Bezier Derivatives",
+    "title": "Bézier Derivatives",
     "category": "section",
-    "text": "The two derivatives could be obtained starting from the original function:P(t) = sum_i=0^n B_iJ_ni(t)P(t) = sum_i=0^n B_iJ_ni(t)where the two derivatives could be obtained with the following formulas:J_ni(t) = frac1-ntt(1-t)J_ni(t)J_ni(t) = frac(i-nt)^2-nt^2-i(1-2t)t^2(1-t)^2 J_ni(t)"
+    "text": "The two derivatives could be obtained starting from the original function:P(t) = sum_i=0^n B_iJ_ni(t)P(t) = sum_i=0^n B_iJ_ni(t)where the two derivatives could be obtained with the following formulas:J_ni(t) = frac1-ntt(1-t)J_ni(t)J_ni(t) = frac(i-nt)^2-nt^2-i(1-2t)t^2(1-t)^2 J_ni(t)defined in all the points but the first and the last (where t = 0 and t = 1)."
+},
+
+{
+    "location": "bezierC.html#Derivatives-Matrix-Representation-1",
+    "page": "Bezier Curves",
+    "title": "Derivatives Matrix Representation",
+    "category": "section",
+    "text": "In order to enhance the matrix representation it is usefull to represent the derivatives in two matrices:Der1 = beginbmatrix\n	frac-nt_(1)t_(1)*(1-t_(1))		dots		dots		dots		fracn-nt_(0)t_(0)(1-t_(0))	\n\n	vdots		ddots						vdots							vdots		vdots	\n	dots		dots		frac(j-1)-nt_(i)t_(i)(1-t_(i))		dots		dots	\n	vdots		vdots						vdots							ddots		vdots	\n\n	frac-nt_(bpts-1)t_(bpts-1)(1-t_(bpts-1))		dots		dots		dots		fracn-nt_(bpts-1)t_(bpts-1)(1-t_(bpts-1)) \nendbmatrixandDer2 = beginbmatrix\n	frac(-nt_(1))^2- nt_(1)^2t_(1)^2*(1-t_(1))^2		dots		dots		dots		frac(n-nt_(0))^2-nt_(0)^2-n(1-2t_(0))t_(0)^2(1-t_(0))^2	\n\n	vdots		ddots										vdots												vdots		vdots	\n	dots		dots		frac((j-1)-nt_(i))^2-nt_(i)^2-(j-1)(1-2t_(i))t_(i)^2(1-t_(i))^2		dots		dots	\n	vdots		vdots										vdots												ddots		vdots	\n\n	frac(-nt_(bpts-1))^2-nt_(bpts-1)^2t_(bpts-1)^2(1-t_(bpts-1))^2		dots		dots		dots		frac(n-nt_(bpts-1))^2-nt_(bpts-1)^2-n(1-2t_(bpts-1))t_(bpts-1)^2(1-t_(bpts-1))^2 \nendbmatrixwhich could be scalar multiplied with the base matrix in order to obtain two derivative base matrices. Of course the T matrix used must be reduced by eliminating the first and the last points.The result of the operation is then:beginsplit\n	P(t) = 	Der1cdot(T_1^dpts-1CD)G \n	P(t) = 	Der2cdot(T_1^dpts-1CD)G"
 },
 
 {
@@ -153,11 +161,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "bezierS.html#Bezier-Surfaces-1",
+    "location": "bezierS.html#Bézier-Surfaces-1",
     "page": "Bezier Surfaces",
-    "title": "Bezier Surfaces",
+    "title": "Bézier Surfaces",
     "category": "section",
-    "text": ""
+    "text": "Bézier Surfaces are special kind of NURBS build upon a controll net B and a couple of Bernstein basis J_ni(u) and K_mj(w).Mathematically, a parametric Bézier surface is defined byQ(u w) = sum_i=0^n sum_j=0^m B_ij J_ni(u) K_mj(w)where u and w are two parametric directions and n = npts - 1 while m = mpts - 1.We point out that the Bernstein Basis are defined asbeginsplit\n	J_n i(u) = binom ni u^i(1-u)^n-i\n	K_m j(w) = binom mj w^j(1-w)^m-j\nendsplit"
+},
+
+{
+    "location": "bezierS.html#Bézier-Properties-1",
+    "page": "Bezier Surfaces",
+    "title": "Bézier Properties",
+    "category": "section",
+    "text": "Many properties of Bézier Surfaces are known:The degree of the surface in each parametric direction is one less than the number of control net vertices in that direction.\nThe continuity of the surface in each parametric direction is two less than the number of control net vertices in that direction.\nThe surface generally follows the shape of the control net.\nOnly the corner points of the control net and the resulting Bézier surface are coincident.\nThe surface is contained within the convex hull of the control net.\nThe surface does not exhibit the variation-diminishing property. The variation-diminishing property for bivariant surfaces is both undefined and unknown.\nThe surface is invariant under an affine transformation."
+},
+
+{
+    "location": "bezierS.html#Matrix-Representation-1",
+    "page": "Bezier Surfaces",
+    "title": "Matrix Representation",
+    "category": "section",
+    "text": "Like in Bézier Curves, also Bézier surfaces are very well described by a matrix point of view. Here we could simplify the calculus by writing:	Q(u w) = UNBM^TW^Twherebeginsplit\n	U = beginbmatrix \n		u^n  u^n-1  dots  1\n	endmatrix\n	W = beginbmatrix \n		w^n  u^w-1  dots  1\n	endmatrix\n	B = beginbmatrix \n		B_00		dots		B_0m	\n		vdots		ddots		vdots	\n		B_n0		dots		B_nm\n	endmatrix\nendsplitand N and M are the coefficient matrix of the base, so given by:	beginsplit\n	N = beginbmatrix\n		binom n0binom nn (-1)^n  binom n1 binomn-1n-1(-1)^n-1  dots  binom nn binomn-nn-n(-1)^0\n		binom n0binom nn-1 (-1)^n-1  binom n1 binomn-1n-2(-1)^n-2  dots  0\n		vdots  vdots  ddots  vdots\n		binom n0binom n1 (-1)^1  binom n1 binomn-10(-1)^0  dots  0\n		binom n0binom n0 (-1)^0  0  dots  0\n	endbmatrix \n	M = beginbmatrix\n		binom m0binom mm (-1)^m  binom m1 binomm-1m-1(-1)^m-1  dots  binom mm binomm-mm-m(-1)^0\n		binom m0binom mm-1 (-1)^m-1  binom m1 binomm-1m-2(-1)^m-2  dots  0\n		vdots  vdots  ddots  vdots\n		binom m0binom m1 (-1)^1  binom m1 binomm-10(-1)^0  dots  0\n		binom m0binom m0 (-1)^0  0  dots  0\n	endbmatrix or, in other worlds:beginsplit\n	left(N_i+1j+1right)_ij=0^n = begincases\n		binom nj binomn-jn-i-j(-1)^n-i-j		mboxif  0 leq i+j leq n\n		0												mboxotherwise\n	endcases\n	left(M_i+1j+1right)_ij=0^m = begincases\n		binom mj binomm-jm-i-j(-1)^m-i-j		mboxif  0 leq i+j leq m\n		0												mboxotherwise\n	endcases\nendsplitof course it is possible to decompose N and M in two more matrices, like for curves, leading to the following scheme:Q(u w) = U(CD)B(EF)^TW^T = UCDBF^TE^TW^TMuch more interesting is the situation where m = n as M = N"
+},
+
+{
+    "location": "bezierS.html#Bézier-Surface-Derivatives-1",
+    "page": "Bezier Surfaces",
+    "title": "Bézier Surface Derivatives",
+    "category": "section",
+    "text": "Of course is simple to determine the derivatives starting from the matrix notation.We recall th results about the first and the second derivatives but the process could be extended further with no particular changes:beginsplit\n	frac partialpartial u Q(uw) = 	UNBM^TW^T\n	frac partialpartial w Q(uw) = 	UNBM^TW^T\n	frac partial^2partial upartial w Q(uw) = 	UNBM^TW^T\n	frac partial^2partial u^2 Q(uw) = 	UNBM^TW^T\n	frac partial^2partial w^2 Q(uw) = 	UNBM^TW^T\nendsplitwhere, as expected, we have:beginsplit\n	U = nu^n-1  (n-1)u^n-2  dots  2n  1  0\n	W = mw^m-1  (m-1)w^m-2  dots  2m  1  0\n	U = n(n-1)u^n-2  (n-1)(n-2)u^n-3  dots  1  0  0\n	W = m(m-1)w^m-2  (m-1)(m-2)w^m-3  dots  1  0  0\nendsplit"
 },
 
 {
